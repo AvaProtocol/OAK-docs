55ca9ece05a38c15c5ed04b84ccfde0712b6c85a
imstar15 <imstar15@qq.com>
2022-05-19 20:04:37 +0800

    apply anchorjs to section title.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,5 @@
 // Custom scripts
+document.addEventListener("DOMContentLoaded", function(event) {
+  anchors.options.placement = 'left'
+  anchors.add('.article-content h1, .article-content h2, .article-content h3, .article-content h4, .article-content h5, .article-content h6');
+});
